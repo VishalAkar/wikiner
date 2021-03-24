5112b28f67586c3fb1aef274df7c82b06b20f498
VishalAkar <vishalakar01@gmail.com>
2021-03-24 16:58:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from numpy.lib.twodim_base import triu_indices
+import wikipedia as wiki
+import spacy 
+from flask import Flask, render_template,url_for,request
+from spacy import displacy
+from flaskext.markdown import Markdown
+
+ner_Obj = spacy.load("en_core_web_sm")
+
+app = Flask(__name__)
+Markdown(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/getData',methods=["POST"])
+def getData():
+    if request.method=="POST":
+        data = request.form['searchtext']
+        datasearch = wiki.page(str(data)).content
+        NerData = ner_Obj(datasearch)
+        result = displacy.render(NerData,style='ent')
+    return render_template('result.html',rawtext= datasearch,result = result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
